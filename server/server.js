0bef91d88b1cc1974b01518fef5427cca44b8b4b
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-28 13:34:34 -0500

    add express static to server files from public directory

--- server/server.js
+++ server/server.js
@@ -7,4 +7,7 @@ app.get('/', (req, res, next) => {
     res.send('Hello from the server side...');
 });
 
+// give express access to all files in public directory
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.listen(3000);
